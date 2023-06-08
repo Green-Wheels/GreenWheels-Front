ffb1eb949612ac3bf467a1213e585af2c47050ba
Gaby <gabo02230223@gmail.com>
2023-06-08 22:49:02 +0200

    fetch auf vehicle geändert nun auf onrender.com

--- src/js/api/vehiclesApi.js
+++ src/js/api/vehiclesApi.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export  const fetchVehicleDetails = async (id) => {
-    try {
-      const response = await axios.get(
-        `http://localhost:8081/vehicles/${id}` // Verwende die Fahrzeug-ID in der URL
-      );
-     return response.data;
-    } catch (error) {
-      console.error("Error fetching vehicle details: ", error);
-    }
-  };
+export const fetchVehicleDetails = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://g5-greenwheels-backend-2ilc.onrender.com/vehicles/${id}` // Verwende die Fahrzeug-ID in der URL
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching vehicle details: ", error);
+  }
+};
